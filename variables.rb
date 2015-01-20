b74e7b9cd394e437db17b98e743cc8e3280c89d0
Taylor Huston <taylor.a.huston@gmail.com>
2015-01-19 21:09:12 -0700

    Add variables.rb

---
+++ variables.rb
@@ -0,0 +1,22 @@
+#strings.rb  - Based on Learn To Program, 2nd Ed, Chapter 4
+
+my_string = '...you can say that again...'
+puts my_string
+
+name = "Taylor Alexander Huston"
+puts 'My name is ' + name + '.'
+
+flexible_type = 'just another'  + ' string'
+puts flexible_type
+flexible_type = 5 * 12
+puts flexible_type
+
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+puts ''
+var1 = 'eight'
+puts var1
+puts var2
