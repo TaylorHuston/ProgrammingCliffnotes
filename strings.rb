137ab295f854d1cefd96c21fdf0fab6e35dcfd73
Taylor Huston <taylor.a.huston@gmail.com>
2015-01-19 21:03:54 -0700

    Add strings.rb

---
+++ strings.rb
@@ -0,0 +1,20 @@
+#strings.rb  - Based on Learn To Program, 2nd Ed, Chapter 3
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts 'I like' + 'apple pie.'
+
+puts 'blink' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'You\'re swell!'
+
