21488397c636a0f25bdf29f24200cc0fe55dd11a
Taylor Huston <taylor.a.huston@gmail.com>
2015-04-18 20:41:52 -0700

    Add notes from Lynda Ruby course

--- variables.rb
+++ variables.rb
@@ -3,38 +3,48 @@
 my_string = '...you can say that again...'
 puts my_string
 
+gets
+
 name = "Taylor Alexander Huston"
-puts 'My name is ' + name + '.'
+puts 'My name is ' + name + '.' #String concatenation
+
+gets
 
 flexible_type = 'just another'  + ' string'
 puts flexible_type
-flexible_type = 5 * 12
+flexible_type = 5 * 12 #Variables are strictly typed
 puts flexible_type
 
+gets
 
 var1 = 8
 var2 = var1
-puts var1
-puts var2
-puts ''
+puts var1 #8
+puts var2 #8
+
+puts
+
 var1 = 'eight'
-puts var1
-puts var2
+puts var1 #eight
+puts var2 #8
+
+gets
 
 var1 = 2
 var2 = '5'
 puts var1.to_s + var2  #Convert var1 to a string
 puts var1 + var2.to_i #Convert var2 to an integer
 
-puts '15'.to_f
-puts '99.999'.to_f
-puts '99.999'.to_i
-puts ''
-puts '5 is a number'.to_i
-puts 'Whatabout 6?'.to_i
-puts 'I will fight you'.to_f
-puts ''
-puts 'string'.to_s
-puts 3.to_i
+gets
+
+puts '15'.to_f    #Convert to float
+puts '99.999'.to_f    #Convert to float
+puts '99.999'.to_i    #Convert to int
+
+puts
+puts '5 is a number'.to_i #Convert to interger (strip off any text)
+puts 'Whatabout 6?'.to_i  #Can't convert, number not first character
+puts 'I will fight you'.to_f  #Nothing to conver
+
 
 
