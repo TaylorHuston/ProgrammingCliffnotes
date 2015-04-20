d7534628bc40994eda965407dace400107b7d1d0
Taylor Huston <taylor.a.huston@gmail.com>
2015-04-19 20:37:55 -0700

    Add Hash notes from Lynda course

--- hashes.rb
+++ hashes.rb
@@ -1,9 +1,9 @@
 #hashes.rb - Demonstration of basic hash syntax
 
 student_ages = {
-    "Jack" => 10,
-    "Jill" => 12,
-    "Bob" => 14
+    :Jack => 10,
+    :Jill => 12,
+    :Bob => 14
   }
 
 puts student_ages.keys
@@ -13,6 +13,10 @@ puts student_ages["Jack"]
 student_ages["Tom"] = 13
 puts student_ages["Tom"]
 
+puts student_ages.to_a.inspect
+
+puts
+
 student_ages.each do |name, age|
-    puts "#{name} is  #{age}'s old"
+    puts "#{name} is  #{age} years old"
 end
