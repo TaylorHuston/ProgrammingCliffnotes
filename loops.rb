757bf3a4d3ca753c331ac2e95df68d7cc6493986
Taylor Huston <taylor.a.huston@gmail.com>
2015-04-22 21:26:44 -0700

    Add loop notes from Lynda course

--- loops.rb
+++ loops.rb
@@ -1,17 +1,9 @@
 #loops.rb - Demonstration of the different loop structres in Ruby
 
-
-
 i = 0
 while i < 5 do
     puts "i " + i.to_s
     i = i + 1
-end
-
-gets
-
-5.times do |y|
-    puts "y " + y.to_s
 end
 
 gets
@@ -24,14 +16,33 @@ end
 
 gets
 
+5.times do |y|
+    puts "y " + y.to_s
+end
+
+gets
+
 array = [1, 2, 3, 4, 5]
 
 for i in array
   puts "array["+i.to_s+"] " + i.to_s
 end
 
-puts
+gets
 
 array.each do |i|
   puts "array["+i.to_s+"] " + i.to_s
 end
+
+gets
+
+x = 0
+loop do
+  x+=1
+  break if x > 10 #Shorthand IF
+  next if x % 2 == 0
+  puts "x " + x.to_s
+end
+
+  
+    
