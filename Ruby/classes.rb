19e8874e6b6d1f8fa3707a25fd6197623f87b5dc
Taylor Huston <taylor.a.huston@gmail.com>
2015-04-28 23:24:46 -0700

    Add Classes notes from Lynda course

--- Ruby/classes.rb
+++ Ruby/classes.rb
@@ -3,9 +3,12 @@
 class Rectangle
   attr_accessor :length, :breadth #Getter and Setter shorthand
   
+  
+  @@total = 0  #Class variable
   def initialize(length, breadth) #Constructor
-    @length = length
+    @length = length  #Instance variable
     @breadth = breadth
+    @@total += 1
   end
 
   def perimeter
@@ -13,34 +16,48 @@ class Rectangle
   end
 
   def area
-    return @length * @breadth
+    @length * @breadth  #Return statement optional
   end
+  
+  #Reader method for class variable
+  def self.total
+    @@total
+  end
+
 end
 
-small_square = Rectangle.new(10,10)
-puts small_square.perimeter.to_s #40
-puts small_square.area.to_s #100
+small_rect = Rectangle.new(10,15)
+puts small_rect.perimeter.to_s #50
+puts small_rect.area.to_s #150
 
-small_square.length = 100
-small_square.breadth = 100
-puts small_square.perimeter #400
+small_rect.length = 100
+small_rect.breadth = 100
+puts small_rect.perimeter #400
 
 #Inheritance
 class Square < Rectangle
+  
  def initialize(length)
    @length = length
    @breadth = length
+   @@total += 1
  end
+
+  
 end
 
 perfect_square = Square.new(20)
-puts perfect_square.perimeter
+puts perfect_square.perimeter #80
+
+puts Rectangle.total #2
 
 #Class with a static method
 class MyClass
+
   def self.cls_method
     return "Static Method"
   end
+
 end
 
 puts MyClass.cls_method
