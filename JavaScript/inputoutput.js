8622810eeba86f1b4e1f1d8ee6e4b002151bab5b
Taylor Huston <taylor.a.huston@gmail.com>
2015-07-16 21:23:41 -0700

    Add more console. options

--- JavaScript/inputoutput.js
+++ JavaScript/inputoutput.js
@@ -1,6 +1,4 @@
-//A basic program going over the different input and output methods
-//Based on http://learn.jquery.com/javascript-101/types/ and
-//Codeacademy
+//Input and output
 
 var inputOutput = function () {
   alert("I am an alert box");
@@ -8,9 +6,16 @@ var inputOutput = function () {
 
   var name = prompt("Use a prompt to get data");
 
+  //Pops up
   alert("Hi " + name);
 
+  //Only visible in debugging tools
   console.log("I'm printed to the console");
   console.log(10 < 5); //Can print boolean
   console.log("blahblahblah".substring(3, 7));
+  //Other console options, don't really do anything differnet, more for self documentation
+  console.error("I am an error");
+  console.info("I'm for general info");
+  console.debug("I am a debug message");
+  console.warn("I am a warning");
 };
