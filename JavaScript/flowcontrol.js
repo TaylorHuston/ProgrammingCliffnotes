1d538af3674192e37234f2a3f1dd169c70fb3da3
Taylor Huston <taylor.a.huston@gmail.com>
2015-07-16 21:37:59 -0700

    Add notes from Lynda course

--- JavaScript/flowcontrol.js
+++ JavaScript/flowcontrol.js
@@ -1,4 +1,4 @@
-//A basic program going over the different conditionals and loops
+//Conditionals and loops
 
 var age = prompt("How old are you?");
 
@@ -8,13 +8,43 @@ if (age > 30) {
   console.log("Yung'in");
 }
 
+if (true && true) {
+  console.log("true");
+}
+
+if (false || true) {
+  console.log("true");
+}
+
+if (true && false) {
+  console.log("false"); //Won't print
+}
+
 console.log(1 == 1); //True
 console.log(1 == "1"); //True, type conversion
 console.log(1 === 1); //True
 console.log(1 === "1"); //False, strict comparison
+console.log(1 != 1); //False
 
+var score1 = 100;
+var score2 = 200;
+//Ternary      *condition* ? *result if true* : *result if false*
+var highScore = (score1 > score2) ? score1 : score2;
+
+
+//Loops
 for (var i = 0; i < 5; i++) {
   console.log(i);
+}
+
+for (var i = 0; i < 5000; i++) {
+  if (i % 2) {
+    continue; //Move to next loop iteration
+  }
+  console.log(i);
+  if (i == 15) {
+    break; //End Loop
+  }
 }
 
 var x = 0;
@@ -24,10 +54,12 @@ while (x < 10) {
 }
 
 do {
-  x--
+  x--;
   console.log(x);
 } while (x > 0);
 
+
+//Switch
 var lunch = "Sammich";
 switch (lunch) {
 case "Sammich":
