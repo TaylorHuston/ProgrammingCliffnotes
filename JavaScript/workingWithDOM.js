633f84061998dc416d4b906aab92272bb549e688
Taylor Huston <taylor.a.huston@gmail.com>
2015-07-17 22:08:02 -0700

    Add DOM manipulation notes from Lynda course

---
+++ JavaScript/workingWithDOM.js
@@ -0,0 +1,25 @@
+//Syntax that targets the DOM
+
+//Grab an element by it's ID
+var myElement = document.getElementById("someParent");
+//Return the 'type' of node
+//1: Element
+//2: Attribute
+//3: Test
+console.log(myElement.nodeType);  //1
+console.log(myElement.innerHTML);
+//Array of any children nodes
+console.log(myElement.childNodes.length);
+
+//Grab an array of all elements by tag
+var myLists = document.getElementsByTagName("li");
+console.log(myLists.length);
+
+//Only li's inside of 'someParent' div
+var someLists = myElement.getElementsByTagName("li");
+console.log(someLists.length);
+
+//You can get and set attributes on elements
+myElement.setAttribute("class", "someClass");
+myElement.setAttribute("align", "left");
+console.log(myElement.getAttribute("class"));
