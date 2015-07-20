e5bf19fa2bea1b165f553fd473bcfdf55a058693
Taylor Huston <taylor.a.huston@gmail.com>
2015-07-19 21:15:41 -0700

    Ser vairbales.js to run on button click

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,37 +1,43 @@
 //A basic program going over the different variable types in JS
 
-//Weakly-typed
-var a = "I am a string";
-var b = 100;
-var c = 100.10;
-var d = true;
+var varButton = document.getElementById("variables");
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+function variables() {
+  //Weakly-typed
+  var a = "I am a string";
+  var b = 100;
+  var c = 100.10;
+  var d = true;
 
-//Change type
-b = a;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
 
-console.log(b);
+  //Change type
+  b = a;
 
-//Random value
-var ran = Math.random(); //Between 0 and 1
+  console.log(b);
+
+  //Random value
+  var ran = Math.random(); //Between 0 and 1
 
 
-var x = 1;
-x++;
-console.log(x);
-x += 10;
-console.log(x);
-x %= 7; //Modulus
-console.log(x);
-console.log(x++); //Logs x first, then increments
-console.log(++x); //Increments, then logs
+  var x = 1;
+  x++;
+  console.log(x);
+  x += 10;
+  console.log(x);
+  x %= 7; //Modulus
+  console.log(x);
+  console.log(x++); //Logs x first, then increments
+  console.log(++x); //Increments, then logs
 
-//Math has other functions
-x = 215.67
-var y = Math.round(x);
-console.log(x); //215.67
-console.log(y); //216
+  //Math has other functions
+  x = 215.67
+  var y = Math.round(x);
+  console.log(x); //215.67
+  console.log(y); //216
+}
+
+varButton.onclick = variables;
