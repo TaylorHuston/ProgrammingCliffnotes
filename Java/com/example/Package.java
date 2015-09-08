df3a7a8a91f563bf98e40204cc9c37e59f32ffc2
Taylor Huston <taylor.a.huston@gmail.com>
2015-09-08 13:14:39 -0700

    Add Package.java

---
+++ Java/com/example/Package.java
@@ -0,0 +1,10 @@
+//Packages are used to organize classes
+package com.example;
+
+public class Package {
+
+  public static void main(String[] args) {
+    System.out.println("I am inside of the example.com package");
+  }
+
+}
