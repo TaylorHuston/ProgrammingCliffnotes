2822c02fc1872b6ad6f623ee9d73c93c8affd0ab
Taylor Huston <taylor.a.huston@gmail.com>
2015-09-08 18:34:10 -0700

    Add notes about Strings from Lynda course

--- Java/Objects.java
+++ Java/Objects.java
@@ -1,6 +1,6 @@
 public class Objects {
 
-  public class main(String[] args) {
+  public static void main(String[] args) {
 
     //Objects
     String name = "Bob";
@@ -10,5 +10,24 @@ public class Objects {
 
     System.out.println(name); //Bob
     System.out.println(myInt); //47
+
+    //Strings have a lot of helper functions
+    System.out.println(name.length()); //3
+    String s1 = name+"by";
+    System.out.println(s1); //Bobby
+    String s2 = s1+" loves chicken";
+    System.out.println(s2); //Bobby loves chikcen
+
+    char[] hello = {'H', 'e', 'l', 'l', 'o'};
+    String s3 = new String(hello);
+    System.out.println(s3); //Hello
+
+    char[] chars = s3.toCharArray();
+    System.out.println(chars); //Hello
+
+    for (char c : chars) { //Each looop
+      System.out.println(c);
+    }
+
   }
 }
