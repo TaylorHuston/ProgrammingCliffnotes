bd7a747b531986b76066b58d2f081fb82d7c4063
Taylor Huston <taylor.a.huston@gmail.com>
2015-09-09 14:39:07 -0700

    Add notes about Loops and Switches from Lynda course

--- Java/FlowControl.java
+++ Java/FlowControl.java
@@ -16,6 +16,46 @@ public class FlowControl {
     String result = (x == y) ? "They are equal" : "They are not equal";
     System.out.println(result);
 
-  }
+    x = 0;
+    //Switch
+    switch (x) {
+      case 0:
+        System.out.println("This should be ran");
+        break;
+      case 1:
+        System.out.println("This won't be ran");
+        break;
+      default:
+        System.out.println("This would be ran if none of the other conditions were met");
+        break;
+    }
 
+    int[] someArray = {1,2,3,4,5,6,7,8,9};
+
+    //For
+    for (int i=0; i < someArray.length; i++) {
+      System.out.print(someArray[i]);
+    }
+    System.out.println();
+    //For, colletions only
+    for (int z : someArray) {
+      System.out.print(z);
+    }
+    System.out.println();
+
+    //While
+    int i = 0;
+    while (i < someArray.length) {
+      System.out.print(someArray[i]);
+      i++;
+    }
+    System.out.println();
+
+    //Do-While
+    i = 0;
+    do {
+      System.out.print(someArray[i]);
+      i++;
+    } while (i < someArray.length);
+  }
 }
