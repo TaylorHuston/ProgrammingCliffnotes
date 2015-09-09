ab3efcb8770c70704452a6f6cffb24f666ca263f
Taylor Huston <taylor.a.huston@gmail.com>
2015-09-08 17:41:53 -0700

    Add 'if' notes to FlowControl

--- Java/FlowControl.java
+++ Java/FlowControl.java
@@ -2,6 +2,20 @@ public class FlowControl {
 
   public static void main(String[] args) {
 
+    int x = 0;
+    int y = 1;
+
+    //Basic if
+    if (x != y) {
+      System.out.println("They are not equal");
+    } else {
+      System.out.println("They are equal");
+    }
+
+    //Ternary shorthand
+    String result = (x == y) ? "They are equal" : "They are not equal";
+    System.out.println(result);
+
   }
 
 }
