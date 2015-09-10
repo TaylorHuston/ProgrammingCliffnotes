720c54e0bde1b98157967eea5a075cf8a2213585
Taylor Huston <taylor.a.huston@gmail.com>
2015-09-09 18:49:34 -0700

    Add notes about Functions from Lynda course

---
+++ Java/Functions.java
@@ -0,0 +1,41 @@
+public class Functions {
+
+  public static void main(String[] args) {
+    int x = 127;
+
+    noParams();
+    oneParam(x);
+    System.out.println(x); //127
+
+    System.out.println(toAdd(12, 43));
+    System.out.println(toAdd(11.1, 11.1, 11.1));
+
+  }
+
+  //Void for no return type
+  static void noParams() {
+    System.out.println("No params");
+  }
+
+  //Primitive variables (and Strings) passed by value
+  static void oneParam(int x) {
+    x++;
+    System.out.println(x); //128
+  }
+
+  //Specify which return type
+  static int toAdd(int x, int y) {
+    return (x+y);
+  }
+
+  //Overload function by changing signature
+  //Can pass in a collection of undetermined size
+  static double toAdd(double ... doubles) {
+    double total = 0;
+    for (double d : doubles) {
+      total += d;
+    }
+    return total;
+  }
+
+}
