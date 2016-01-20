efaf037e1a7decc552ebed75af21765b0425df84
Taylor Huston <taylor.a.huston@gmail.com>
2016-01-19 23:19:22 -0700

    Rename files

--- JavaScript/AngularJS/AngularDemoModule.js
+++ JavaScript/AngularJS/AngularDemoModule.js
@@ -1,7 +1,18 @@
-var app = angular.module('demoApp', []);
+var demoApp = angular.module('demoApp', []); //Empty array is for dependencies
 
-app.controller('demoController', function() {
-
+demoApp.controller('demoController', function ($scope) {
+  function SomeController($scope) {
+    $scope.people = [{
+      name: 'Al',
+      state: 'Oklahoma'
+      }, {
+      name: 'Bob',
+      state: 'Alaska'
+      }, {
+      name: 'Cat',
+      state: 'Colorado'
+      }]
+  }
 
 
 });
