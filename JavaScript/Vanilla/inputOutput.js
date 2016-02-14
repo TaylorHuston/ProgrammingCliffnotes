ebaaaad3dc8b70161d0c89f84c7d5b2a2f807fdf
Taylor Huston <taylor.a.huston@gmail.com>
2016-02-13 23:19:54 -0700

    Improve notes on input and output

--- JavaScript/Vanilla/inputOutput.js
+++ JavaScript/Vanilla/inputOutput.js
@@ -1,21 +1,26 @@
 //Input and output
 
+var inputButton = document.getElementById("inputoutput");
+
 var inputOutput = function () {
   alert("I am an alert box");
   confirm("I am a confirm box, click okay to continue");
 
-  var name = prompt("Use a prompt to get data");
+  var someVal = prompt("Use a prompt to get data");
 
   //Pops up
-  alert("Hi " + name);
+  alert("You input " + someVal);
 
   //Only visible in debugging tools
   console.log("I'm printed to the console");
   console.log(10 < 5); //Can print boolean
   console.log("blahblahblah".substring(3, 7));
-  //Other console options, don't really do anything differnet, more for self documentation
+
+  //Other console options, don't really do anything different, more for self documentation
   console.error("I am an error");
   console.info("I'm for general info");
   console.debug("I am a debug message");
   console.warn("I am a warning");
 };
+
+inputButton.onclick = inputOutput;
