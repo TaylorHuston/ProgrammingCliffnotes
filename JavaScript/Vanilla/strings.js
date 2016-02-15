10dc607decbc3cc93b550516e1b369baca5df402
Taylor Huston <taylor.a.huston@gmail.com>
2016-02-14 23:13:17 -0700

    Improce String notes

--- JavaScript/Vanilla/strings.js
+++ JavaScript/Vanilla/strings.js
@@ -1,29 +1,35 @@
 //Basic string functions
 
-var s = "Supercalifragilisticexpealadocious";
-console.log(s);
-console.log(s.length);
+var stringButton = document.getElementById("strings");
 
-var formatted = "Quoth the raven:\n\t\"Nevermore!\"";
-console.log(formatted);
+function strings() {
+  var s = "Supercalifragilisticexpealadocious";
+  console.log(s);
+  console.log(s.length); //34
 
-//String have a variety of self-explanatory methods
-var alpha = "abcdefghijklmnopqrstuvwxyz"
-console.log(alpha.indexOf("e")); //First occurence
-console.log(alpha.indexOf("DDDD")); //Returns -1
-console.log(alpha.charAt(11)); //l
-console.log(alpha.toUpperCase());
-console.log(alpha.slice(12, 18)); //Between position from 12 to 17 (doesn't include 18)
-console.log(alpha.replace("lmnop", "ponml"));
+  var formatted = "Quoth the raven:\n\t\"Nevermore!\"";
+  console.log(formatted);
 
-var phrase = "This is a phrase";
-var words = phrase.split(" "); //Splits at a space into an array
-console.log(words);
+  //String have a variety of self-explanatory methods
+  var alpha = "abcdefghijklmnopqrstuvwxyz"
+  console.log(alpha.indexOf("efg")); //First occurence, 4
+  console.log(alpha.indexOf("DDDD")); //Returns -1
+  console.log(alpha.charAt(11)); //l
+  console.log(alpha.toUpperCase());
+  console.log(alpha.slice(12, 18)); //Between position from 12 to 17 (doesn't include 18)
+  console.log(alpha.replace("lmnop", "ponml"));
 
-//Comparison is case-sensitive
-var str1 = "aardvark";
-var str2 = "beluga";
-var str3 = "Beluga";
+  var phrase = "This is a phrase";
+  var words = phrase.split(" "); //Splits at a space into an array
+  console.log(words);
 
-console.log(str1 < str2); //True
-console.log(str1 < str3); //False
+  //Comparison is case-sensitive
+  var str1 = "aardvark";
+  var str2 = "beluga";
+  var str3 = "Beluga";
+
+  console.log(str1 < str2); //True
+  console.log(str1 < str3); //False
+}
+
+stringButton.onclick = strings;
