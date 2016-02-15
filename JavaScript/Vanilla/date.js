bc219f0ef9dae7787784cf81d7bf1284f60db40d
Taylor Huston <taylor.a.huston@gmail.com>
2016-02-14 23:24:14 -0700

    Improve date notes

--- JavaScript/Vanilla/date.js
+++ JavaScript/Vanilla/date.js
@@ -1,22 +1,28 @@
 //Date object syntax
+var dateButton = document.getElementById("dates");
 
-var today = new Date(); //Current date and time
+function dates() {
+  var today = new Date(); //Current date and time
 
-console.log(today);
+  console.log(today);
 
-var y2k = new Date(2000, 0, 1); //Months are 0-index
+  var y2k = new Date(2000, 0, 1); //Months are 0-index
 
-//Dates have a rang of self-explanatory methods
-console.log(y2k.getMonth());
-console.log(y2k.getDate()); //Day of month (1-31)
-console.log(y2k.getDay()); //Day of week (0-6)
-console.log(y2k.getTime()); //Milliseconds since 1/1/1970
+  //Dates have a rang of self-explanatory methods
+  console.log(y2k.getMonth());
+  console.log(y2k.getDate()); //Day of month (1-31)
+  console.log(y2k.getDay()); //Day of week (0-6)
+  console.log(y2k.getFullYear());
+  console.log(y2k.getTime()); //Milliseconds since 1/1/1970
 
-today.setMonth(5);
-console.log(today);
+  today.setMonth(5);  //Can set the values individually
+  console.log(today);
 
-var date1 = new Date(2000, 0, 1);
-var date2 = new Date(2000, 0, 1);
+  var date1 = new Date(2000, 0, 1);
+  var date2 = new Date(2000, 0, 1);
 
-console.log(date1 == date2) //False, two different objects
-console.log(date1.getTime() == date2.getTime()) //True
+  console.log(date1 == date2) //False, two different objects
+  console.log(date1.getTime() == date2.getTime()) //True
+}
+
+dateButton.onclick = dates;
