ce1983e951fb202a749a60264a39ffe1f7b4dfd8
Taylor Huston <taylor.a.huston@gmail.com>
2019-08-10 14:26:53 -0700

    Math examples

--- Java/MathExamples.java
+++ Java/MathExamples.java
@@ -6,12 +6,16 @@ public class MathExamples
         System.out.println(Math.ceil(9.8)); //10 round to next integer
         System.out.println(Math.floor(9.8)); //9
         System.out.println(Math.cos(0.0));  //1.0 cosign
-        System.out.println(Math.sin(0.0))l //0.0 Sine
+        System.out.println(Math.sin(0.0)); //0.0 Sine
         System.out.println(Math.tan(0.0)); //0.0 tangent
         System.out.println(Math.exp(1.0)); //2.71828 exponential method
         System.out.println(Math.max(14,17)); //17
         System.out.println(Math.min(14,17)); //14
         System.out.println(Math.pow(2.0,7.0)); //128 x raised to the y
         System.out.println(Math.sqrt(900.00)); //30 square root
-        System.out.println(Math.log())
+        System.out.println(Math.log(Math.E)); //1 logarithms
+        System.out.println(Math.PI); //Pi
+        System.out.println(Math.E);  //Base value for natural logarithms
     }
+
+}
