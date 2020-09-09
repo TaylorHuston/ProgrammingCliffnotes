2cc8cb94f885fffcba5aab791040af12e15e1920
Taylor Huston <taylor.a.huston@gmail.com>
2020-09-09 13:53:06 -0400

    Add some notes on basic math and comparisons

--- Python3/variables.py
+++ Python3/variables.py
@@ -13,6 +13,7 @@ str2 = "12"
 print(str1+" "+str2) #Hello 12
 
 #print(num1+str2)  this will throw an error
+#print(str1-str2)  this will throw an error
 
 print(str1[0]) #H
 print(str1[1:5]) #ello
@@ -27,3 +28,42 @@ print(myList1+myList2) #[1, 2.3, 3, 4, 'bob', 17, '34']
 
 myList1[3] = 34234
 print(myList1[3]) #34234
+
+myTuple = (123131, 12321321, 1231) #tuples are not mutable
+print(myTuple) #(123131, 12321321, 1231)
+print(myTuple[1]) #12321321
+
+myDict = {'Name' : 'Tom', 'Height': 6.2}
+print(myDict)  #{'Name': 'Tom', 'Height': 6.2}
+print(myDict['Name'])  #Tom
+myDict['Name'] = "Jerry"
+print(myDict['Name'])  #Jerry
+
+myDict.clear()
+print(myDict) #{}
+del myDict
+#print(myDict)  this will throw an error
+
+
+#Math
+x=10
+y=3
+
+print(x+y)  #13
+print(x-y)  #7
+print(x*y)  #30
+print(x%y)  #1
+print(x**y)  #1000
+print(x/y)  #3.33333333.....
+print(x//y)  #3
+
+#Compare
+print(x == y) #False
+print(x != y) #True
+print(x < y) #False
+print(x > y)  #True
+print(x <= y)  #False
+print(x >= y) #True
+print(str1 == str2) #False
+print(str1 != str2) #True
+print(str1 > str2)  #True (string length)
