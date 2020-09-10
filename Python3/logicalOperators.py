f4b9640a5af40e28180c2f53a148331942e85ab3
Taylor Huston <taylor.a.huston@gmail.com>
2020-09-10 15:22:57 -0400

    Logical notes

---
+++ Python3/logicalOperators.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+print((4>5) or (4==4))  #True
+print((4>5) and (4==4))  #False
+print(not(4>5))  #True
+print(5 is 5)  #True
+print(5 is not 5)  #False
+
+myList = [12, 43, "Bob", 17]
+print(12 in myList) #True
+print(342 not in myList)  #True
