8f4e0c578d1ba1e3a9fd4a647eaab3b00647882b
Taylor Huston <taylor.a.huston@gmail.com>
2023-02-14 18:49:11 -0500

    Strings

---
+++ Python3/strings.py
@@ -0,0 +1,23 @@
+str1 = "some string"
+
+print(str1.title())
+print(str1.upper())
+print(str1.lower())
+
+#f-strings
+first_name = "john"
+last_name = "doe"
+full_name = f"{first_name} {last_name}"
+print(full_name) #john doe
+print(f"Hello, {full_name.title()}") #Hello, John Doe
+
+print("\tPyton") #tab
+print("Languages:\nPython\nC\nJavaScript") #New line
+print("Python ".rstrip()) #Strip right whitespace
+print("  Python".lstrip()) #Strip left whitespace
+
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix('https://')) #nostarch.com
+
+some_file = "python_notes.txt"
+print(some_file.removesuffix(".txt"))
