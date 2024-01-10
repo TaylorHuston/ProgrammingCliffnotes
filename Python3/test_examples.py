2259b7bc426051a79ed91c8e0183d8d99b36358d
Taylor Huston <taylor.a.huston@gmail.com>
2024-01-10 16:48:33 -0500

    Add Python3 test_examples.py

---
+++ Python3/test_examples.py
@@ -0,0 +1,13 @@
+from pytest import skip
+
+def test_true():
+    assert True
+
+def test_false():
+    assert (1+1==3)
+
+def test_addition():
+    assert (1+1==2)
+
+def test_skip():
+    skip() #This test will be skipped
