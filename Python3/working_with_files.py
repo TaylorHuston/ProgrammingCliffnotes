451054cd15b6015cd246bd2217d2bdba5350bce4
Taylor Huston <taylor.a.huston@gmail.com>
2024-01-21 15:55:52 -0500

    Add basic exmaple of writing to and readinf from a file

---
+++ Python3/working_with_files.py
@@ -0,0 +1,15 @@
+with open("test_output.txt", "w") as file:
+    file.write("This is a test file.\n")
+    file.write("This is the second line.\n")
+    file.write("This is the third line.\n")
+    file.writelines(["This is the fourth line.\n", "This is the fifth line.\n"])
+
+with open("test_output.txt", "r") as file:
+    print(file.read()) #print entire file
+    
+    #Needs work below
+    #print(file.readlines()) #print first line
+    
+    
+    #lines = [line for line in file.readlines()]
+    #print(lines)
