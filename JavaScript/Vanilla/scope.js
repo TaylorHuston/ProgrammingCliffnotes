4d2013d2b43e5f70be62d16118cc369bbbea65cd
Taylor Huston <taylor.a.huston@gmail.com>
2025-06-26 16:09:08 -0400

    Add closure examples

--- JavaScript/Vanilla/scope.js
+++ JavaScript/Vanilla/scope.js
@@ -2,10 +2,10 @@ const a = 4; // a is a global variable, it can be accessed by the functions belo
 
 function foo() {
   const b = a * 3; // b cannot be accessed outside foo function, but can be accessed by functions
-  // defined inside foo
+  // Defined inside foo
   function bar(c) {
-    const b = 2; // another `b` variable is created inside bar function scope
-    // the changes to this new `b` variable don't affect the old `b` variable
+    const b = 2; // Another `b` variable is created inside bar function scope
+    // The changes to this new `b` variable don't affect the old `b` variable
     console.log(a, b, c);
   }
 
@@ -25,10 +25,40 @@ function run() {
     var moo = "Mooo";
     let baz = "Bazz";
     console.log(moo, baz); // Mooo Bazz
+    console.log(foo, bar); // Foo Bar
   }
 
   console.log(moo); // Mooo
-  console.log(baz); // ReferenceError
+  //console.log(baz); // ReferenceError
 }
+
+// Closures
+function makeCounter() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+counter1(); // 1
+counter1(); // 2
+
+counter2(); // 1 ← separate memory!
+
+function makeGreeter(name) {
+  return function () {
+    console.log("Hello, " + name + "!");
+  };
+}
+
+const greeter1 = makeGreeter("Taylor");
+const greeter2 = makeGreeter("Alex");
+
+greeter1(); // "Hello, Taylor!"
+greeter2(); // "Hello, Alex!"
 
 run();
