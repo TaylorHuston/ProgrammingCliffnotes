262841f5b1c21ed6d088773995251f2c3dbd4c6d
Taylor Huston <taylor.a.huston@gmail.com>
2025-06-26 16:09:25 -0400

    Fix formatting issues and add missing semicolons in functions.js

--- JavaScript/Vanilla/functions.js
+++ JavaScript/Vanilla/functions.js
@@ -1,7 +1,6 @@
 //Functions
 
 function functions() {
-
   //Standard functions are hoisted when the file is first loaded, can be used before they are defined
   var x = makeANumber();
   console.log(x); //72
@@ -10,12 +9,12 @@ function functions() {
     return 72;
   }
 
-  var name = "Taylor"
+  var name = "Taylor";
 
   //Anonymous functions are not hoisted, must be defined in the code before they are used
   var sayHi = function (name) {
     console.log("Nice to meet you " + name);
-  }
+  };
 
   sayHi(name);
 
@@ -31,7 +30,7 @@ function functions() {
 
   var adder = function (x, y) {
     return x + y;
-  }
+  };
 
   var z = adder(2, 3);
   console.log(z); //5
@@ -40,7 +39,7 @@ function functions() {
   function multiplier(factor) {
     return function (number) {
       return number * factor;
-    }
+    };
   }
 
   var twice = multiplier(2); //Returns an instance of the nexted function above with number = 2;
@@ -92,9 +91,11 @@ function functions() {
   }
 
   //Pass anonymous function to filter, in this case one that test for even numbers
-  console.log(filter(anotherArray, function (number) {
-    return (number % 2 == 0);
-  }));
+  console.log(
+    filter(anotherArray, function (number) {
+      return number % 2 == 0;
+    })
+  );
 
   //Apply allows you to call a function and specify the 'this', followed by an array of args
   function speak(x, y) {
@@ -102,15 +103,46 @@ function functions() {
   }
 
   var someObject = {
-    type: "Human"
+    type: "Human",
   };
 
   speak.apply(someObject, [10, 12]);
 
   //Call is similar, but doesn't take the arguments as an array
-  speak.call({
-    type: "Cat"
-  }, "Hello", 47);
+  speak.call(
+    {
+      type: "Cat",
+    },
+    "Hello",
+    47
+  );
+
+  // Arrow functions are a more concise syntax for writing functions, they do not have their own 'this' context
+  const square = (x) => {
+    return x * x;
+  };
+
+  const person1 = {
+    name: "Taylor",
+    sayHi: function () {
+      console.log("Hi, my name is", this.name);
+    },
+  };
+
+  person1.sayHi(); // "Hi, my name is Taylor"
+
+  const person2 = {
+    name: "Taylor",
+    sayHi: () => {
+      console.log("Hi, my name is", this.name);
+    },
+  };
+
+  person2.sayHi(); // "Hi, my name is undefined"
+  // In this case, 'this' does not refer to the person object, but to the global context (or undefined in strict mode)
+  // Arrow functions do not have their own 'this', they inherit it from the surrounding lexical scope
+  // This means that if you use an arrow function inside a method, it will not have
+  // access to the object it is defined in, which can lead to unexpected behavior.
 }
 
 functions();
