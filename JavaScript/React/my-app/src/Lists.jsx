11a9b6bd158ddcd0246f1a65b5fea3a6bf9b6f05
Taylor Huston <taylor.a.huston@gmail.com>
2025-09-08 16:21:13 -0400

    List example

---
+++ JavaScript/React/my-app/src/Lists.jsx
@@ -0,0 +1,25 @@
+const products = [
+  { title: "Cabbage", isFruit: false, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  // Need to assign a key prop when creating lists of elements in React
+  // so React can keep track of each element between renders.
+  // The key should be a string or number that is unique among its siblings.
+  // Here, we use the product ID, which is unique.
+  // Do not use indexes as keys if the list can be reordered or changed.
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? "magenta" : "darkgreen",
+      }}
+    >
+      {product.title}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}
--- JavaScript/React/my-app/src/main.jsx
+++ JavaScript/React/my-app/src/main.jsx
@@ -5,11 +5,13 @@ import App from "./App.jsx";
 import MyButtonComponent from "./MyButton.jsx";
 import Profile from "./Profile.jsx";
 import Conditional from "./Conditional.jsx";
+import ShoppingList from "./Lists.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Conditional />
     <MyButtonComponent />
     <Profile />
+    <ShoppingList />
   </StrictMode>
 );
