1c55a4bde746d956ba74d41d6e3b85c1045033f4
Taylor Huston <taylor.a.huston@gmail.com>
2025-12-31 16:04:10 -0500

    Add bifs.py with examples of built-in functions in Python

---
+++ Python3/bifs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+Miscellaneous built-in functions (BIFs) in Python.
+"""
+
+print(dir(set()))  # Lists all methods and attributes of set objects
+print(help(set))  # Detailed help on set objects
+print(help(set.add))  # Detailed help on the add method of set objects
